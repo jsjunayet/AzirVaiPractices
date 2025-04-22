0726d6d9b41283ec0493cf2a3eb11e09ba7207e1
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2025-04-22 17:08:58 +0600

    update code

--- js.js
+++ js.js
@@ -1,2 +1,26 @@
+const myset = new Set()
+myset.add("a")
+myset.add("a")
+myset.add("b")
+myset.add("c")
+console.log(typeof myset);
+console.log(myset instanceof Set);
+for(let x of myset){
+    console.log(x);
+}
+const values = myset.values()
+for(let x of values){
+    console.log(x);
+}
+console.log(values);
+const keys = myset.keys()
+console.log(keys);
 
-console.log(Array.isArray(array));
+const map = new Map()
+map.set(100,200)
+console.log(map);
+console.table(
+[map.get(100),map.size]
+);
+
+
