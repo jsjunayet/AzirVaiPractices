c3f07fffe0db87e4b163a3ff611c1d55046dfe72
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2025-04-23 20:26:50 +0600

    update code

--- js.js
+++ js.js
@@ -1,29 +1,7 @@
-const myset = new Set()
-myset.add("a")
-myset.add("a")
-myset.add("b")
-myset.add("c")
-console.log(typeof myset);
-console.log(myset instanceof Set);
-for(let x of myset){
-    console.log(x);
+const text ={
+    "employees":[
+  {"firstName":"John", "lastName":"Doe"},
+  {"firstName":"Anna", "lastName":"Smith"},
+  {"firstName":"Peter", "lastName":"Jones"}
+]
 }
-const values = myset.values()
-for(let x of values){
-    console.log(x);
-}
-console.log(values);
-const keys = myset.keys()
-console.log(keys);
-
-const map = new Map()
-map.set(100,200)
-console.log(map);
-console.table(
-[map.get(100),map.size]
-);
-console.log();  
-let person = {frist:"jnuaue"}
-person =undefined
-console.log(person);
-
