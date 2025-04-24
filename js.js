d33bef325f8508a837fff6df2b6f9593bd58bc3a
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2025-04-24 18:25:08 +0600

    mutable and immutable concept

--- js.js
+++ js.js
@@ -5,3 +5,16 @@ const text ={
   {"firstName":"Peter", "lastName":"Jones"}
 ]
 }
+
+
+const person ={
+  name:'Rahim'
+}
+let person2 = {...person}
+person2.name="junayet"
+console.log(person.name, person2.name
+);
+let immutable = "junayet"
+let mutable = immutable
+mutable ='Rahim'
+console.log(immutable, mutable);
