ebddfd91c5622334efb11cac45f04669f0168d13
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2025-04-25 05:39:53 +0600

    invocation vs call

--- js.js
+++ js.js
@@ -1,32 +1,38 @@
-const text ={
-    "employees":[
-  {"firstName":"John", "lastName":"Doe"},
-  {"firstName":"Anna", "lastName":"Smith"},
-  {"firstName":"Peter", "lastName":"Jones"}
-]
-}
-
-
-const person ={
-  name:'Rahim'
-}
-let person2 = {...person}
-person2.name="junayet"
-console.log(person.name, person2.name
-);
-let immutable = "junayet"
-let mutable = immutable
-mutable ='Rahim'
-console.log(immutable, mutable);
-
-// let users = 
-//   {  age: 25,name: "Rahim" }
-
-// for (let user of users) {
-//   console.log(user); 
+// const text ={
+//     "employees":[
+//   {"firstName":"John", "lastName":"Doe"},
+//   {"firstName":"Anna", "lastName":"Smith"},
+//   {"firstName":"Peter", "lastName":"Jones"}
+// ]
 // }
-let person5 = {
-  name: "Rahim",
-  age: 25
-};
-console.log(Object.values(person5));
+
+
+// const person ={
+//   name:'Rahim'
+// }
+// let person2 = {...person}
+// person2.name="junayet"
+// console.log(person.name, person2.name
+// );
+// let immutable = "junayet"
+// let mutable = immutable
+// mutable ='Rahim'
+// console.log(immutable, mutable);
+
+// // let users = 
+// //   {  age: 25,name: "Rahim" }
+
+// // for (let user of users) {
+// //   console.log(user); 
+// // }
+// let person5 = {
+//   name: "Rahim",
+//   age: 25
+// };
+// console.log(Object.values(person5));
+
+ function a(){
+  console.log("hello world");
+}
+const x = Math.max.apply(null, [1,2,3]);
+console.log(x);
